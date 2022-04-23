0684e21961a8f1511168839db14d5aece5d6e9ca
Coach <95095309+Fixsbreaker@users.noreply.github.com>
2022-04-23 23:45:07 +0600

    основной запуск

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='templates')
+
+app = Flask(__name__)
 
 
-@app.route('/')
-@app.route('/index')
-def index():
+@app.route('/menu')
+def explain():
     return render_template('index.html')
+
+
+@app.route('/products')
+def all_products():
+    return render_template('products.html')
+
+
+@app.route('/detail')
+def login():
+    return render_template('product-detail.html')
+
+
+@app.route('/reg')
+def registrate():
+    return render_template('log.html')
 
 
 if __name__ == '__main__':
