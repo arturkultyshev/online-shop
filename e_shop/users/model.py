2ce2dfda0845f694b567aee5f1ca3c8ffaa29ca9
arturkultyshev <kultyshev.artur@gmail.com>
2022-05-04 23:10:27 +0600

    создание модели для формирования заказа

--- e_shop/users/model.py
+++ e_shop/users/model.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from flask_login import UserMixin
 
 
-
 @login_manager.user_loader
 def user_loader(user_id):
     return Register.query.get(user_id)
@@ -11,6 +10,7 @@ def user_loader(user_id):
 @login_manager.user_loader
 def user_loader(user_id):
     return Register.query.get(user_id)
+
 #создание бд строк для регистрации. Столбцы:айди, имя, логин, почта, пароль, дата создания
 class Register(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key= True)
@@ -20,7 +20,35 @@ class Register(db.Model, UserMixin):
     password = db.Column(db.String(200), unique= False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
+
     def __repr__(self):
         return '<Register %r>' % self.name
+
+
+class JsonEcodedDict(db.TypeDecorator):
+    impl = db.Text
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return '{}'
+        else:
+            return json.dumps(value)
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return {}
+        else:
+            return json.loads(value)
+
+#создание модели для формирования заказа
+class CustomerOrder(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    invoice = db.Column(db.String(20), unique=True, nullable=False)
+    status = db.Column(db.String(20), default='Pending', nullable=False)
+    customer_id = db.Column(db.Integer, unique=False, nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    orders = db.Column(JsonEcodedDict)
+
+
+    def __repr__(self):
+        return'<CustomerOrder %r>' % self.invoice
 
 db.create_all()
