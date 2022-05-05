a6e85d7aee9804d9b702c488f7e17ab5a24db225
Beka5590 <96657527+Beka5590@users.noreply.github.com>
2022-05-05 16:51:08 +0600

    добавил функции к корзине
    Вчера забыл запушить остался в виде коммита. Создал функции получить товар, обновление  и его удаление, а также очистка всех продуктов

--- e_shop/busket/busket.py
+++ e_shop/busket/busket.py
@@ -44,3 +44,68 @@ def AddCart():
 
     except Exception as e:
         print(e)
+        
+        
+@app.route('/carts')
+def getCart():
+    if 'Shoppingcart' not in session or len(session['Shoppingcart']) <= 0:
+        return redirect(url_for('home'))
+    subtotal = 0
+    grandtotal = 0
+    for key,product in session['Shoppingcart'].items():
+        discount = (product['discount']/100) * float(product['price'])
+        subtotal += float(product['price']) * int(product['quantity'])
+        subtotal -= discount
+        tax =("%.2f" %(.06 * float(subtotal)))
+        grandtotal = float("%.2f" % (1.06 * subtotal))
+    return render_template('products/carts.html', tax=tax,
+                           grandtotal=grandtotal,
+                           brands=brands(),
+                           categories=categories())
+
+
+
+@app.route('/updatecart/<int:code>', methods=['POST'])
+def updatecart(code):
+    if 'Shoppingcart' not in session or len(session['Shoppingcart']) <= 0:
+        return redirect(url_for('home'))
+    if request.method == "POST":
+        quantity = request.form.get('quantity')
+        color = request.form.get('color')
+        try:
+            session.modified = True
+            for key , item in session['Shoppingcart'].items():
+                if int(key) == code:
+                    item['quantity'] = quantity
+                    item['color'] = color
+                    flash('Item is updated!')
+                    return redirect(url_for('getCart'))
+        except Exception as e:
+            print(e)
+            return redirect(url_for('getCart'))
+
+
+
+@app.route('/deleteitem/<int:id>')
+def deleteitem(id):
+    if 'Shoppingcart' not in session or len(session['Shoppingcart']) <= 0:
+        return redirect(url_for('home'))
+    try:
+        session.modified = True
+        for key , item in session['Shoppingcart'].items():
+            if int(key) == id:
+                session['Shoppingcart'].pop(key, None)
+                return redirect(url_for('getCart'))
+    except Exception as e:
+        print(e)
+        return redirect(url_for('getCart'))
+
+
+@app.route('/clearcart')
+def clearcart():
+    try:
+        session.pop('Shoppingcart', None)
+        return redirect(url_for('home'))
+    except Exception as e:
+        print(e)
+
