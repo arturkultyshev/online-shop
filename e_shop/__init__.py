9b25d4e456eb20629b0f108e0f21ffcecf55d3f9
vibe-reverb <85639083+vibe-reverb@users.noreply.github.com>
2022-05-07 15:15:05 +0600

    смог сделать .env добавил его в гит игнор

--- e_shop/__init__.py
+++ e_shop/__init__.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_uploads import UploadSet, configure_uploads, IMAGES,\
-    patch_request_class
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
 import os
 from flask_msearch import Search
 from flask_login import LoginManager
-# from dotenv import dotenv_values, load_dotenv
+from dotenv import load_dotenv
 
 
 app = Flask(__name__)
+load_dotenv()
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 search = Search()
@@ -18,24 +18,21 @@ search.init_app(app)
 # запрашивает местоположение файла(адрес), в котором выполняется код
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
-app.config['SECRET_KEY'] = '27hkmgBJvmvQdIC1poA2W3Cr99n_2zpmNqhKTn7tuds4TWCck'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(BASEDIR, 'static/images')
+app.config['SQLALCHEMY_DATABASE_URI'] = f'{os.getenv("SQLALCHEMY_URI")}'
+app.config['SECRET_KEY'] = f'{os.getenv("SECRET_K")}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = f'{os.getenv("SQLALCHEMY_TRACK_MOD")}'
+app.config['UPLOADED_PHOTOS_DEST'] = f'{os.getenv("UPLOADED_PHOTOS_D")}'
+
 
 photos = UploadSet('photos', IMAGES)
 configure_uploads(app, photos)
 patch_request_class(app)
-# load_dotenv('e_shop/.env')
-# key = app.dotenv_values(".env")["SECRET_KEY"]
-# sql_alchemy_database = app.dotenv_values(".env")["SQLALCHEMY_DATABASE_URI"]
-# sql_alchemy_track = app.dotenv_values(".env")["SQLALCHEMY_TRACK_MODIFICATIONS"]
-# photos_upload = app.dotenv_values(".env")["UPLOADED_PHOTOS_DEST"]
+
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'customerLogin'
-login_manager.needs_refresh_message_category = 'danger'
+login_manager.login_view ='customerLogin'
+login_manager.needs_refresh_message_category='danger'
 login_manager.login_message = u"Please login first"
 
 
@@ -43,3 +40,4 @@ from e_shop.products import routes
 from e_shop.admin import routes
 from e_shop.busket import busket
 from e_shop.users import routes
+
